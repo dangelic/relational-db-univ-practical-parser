d66c02562dd40c5c9105b4b2854256d227dbd30e
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-05-13 09:40:47 +0200

    #refactor Update ETL process caller

--- src/ETLProcess.java
+++ src/ETLProcess.java
@@ -1,13 +1,44 @@
 import com.parser.XMLParser;
+import com.mapper.ObjectMapper;
 
 public class ETLProcess {
 
     public static void main(String[] args) {
-        XMLParser parser = new XMLParser();
-
         try {
-            parser.parseXML("./data/raw/xml/leipzig_transformed.xml", XMLParser.getFile1Attributes(), XMLParser.EXTRACT_FAILS_LOG, "file1.json");
-            parser.parseXML("./data/raw/xml/dresden.xml", XMLParser.getFile2Attributes(), XMLParser.EXTRACT_FAILS_LOG, "file2.json");
+            parseLeipzigXML();
+            parseDresdenXML();
+
+            // Call the ObjectMapper class
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.mapAttributes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseLeipzigXML() {
+        XMLParser parser = new XMLParser();
+        try {
+            parser.parseXML(
+                    "./data/raw/xml/leipzig_transformed.xml",
+                    XMLParser.getFile1Attributes(),
+                    "./etl/extract_fails/leipzig_extract_fails.log",
+                    "./etl/parsed_to_json_unmapped/leipzig_parsed_unmapped.json"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parseDresdenXML() {
+        XMLParser parser = new XMLParser();
+        try {
+            parser.parseXML(
+                    "./data/raw/xml/dresden.xml",
+                    XMLParser.getFile2Attributes(),
+                    "./etl/extract_fails/dresden_extract_fails.log",
+                    "./etl/parsed_to_json_unmapped/dresden_parsed_unmapped.json"
+            );
         } catch (Exception e) {
             e.printStackTrace();
         }
