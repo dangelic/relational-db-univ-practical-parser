b89c16f85a7b81841dee787de6681fe9c1777107
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-06-28 11:39:06 +0200

    #feat Update example_average_rating.sql

--- sql/example_average_rating.sql
+++ sql/example_average_rating.sql
@@ -1,15 +1,15 @@
--- Example for average_rating calculation via triggered function.
+-- Beispiel für die Berechnung des Durchschnittsratings über einen Trigger-Funktion.
 
--- Select product with asin = B00068C824 -> It has a rating of 3.5 initially, calculated as average from the union of guest- and userreviews.
+-- Wähle das Produkt mit der asin = B00068C824 aus -> Es hat anfangs ein Rating von 3,5, das als Durchschnitt aus den Gast- und Benutzerbewertungen berechnet wird.
 SELECT asin, ptitle, average_rating FROM products WHERE asin LIKE 'B00068C824';
 
--- Insert a positive 5* review (exemplary in guestreviews) and see the average_rating rising.
+-- Füge eine positive 5*-Bewertung ein (exemplarisch in den Gastbewertungen) und beobachte, wie sich das Durchschnittsrating erhöht.
 INSERT INTO guestreviews (guestreview_id, products_asin, rating, helpful_votes, summary, content, review_date)
 VALUES ('123456789', 'B00068C824', 5, 999, 'WOW!', 'This product has me feeling like I am on a Kylie Minogue concert stage—confident, glamorous, and ready to shine brighter than a disco ball!', NOW());
 
 SELECT asin, ptitle, average_rating FROM products WHERE asin LIKE 'B00068C824';
 
--- Insert a negative 1* review (exemplary in userreviews) and see the average_rating shrinking.
+-- Füge eine negative 1*-Bewertung ein (exemplarisch in den Userbewertungen) und beobachte, wie sich das Durchschnittsrating verringert.
 INSERT INTO userreviews (userrreview_id, users_username, products_asin, rating, helpful_votes, summary, content, review_date)
 VALUES ('987654321','sunny82', 'B00068C824', 1, 0, 'Absurdity...', 'Listening to that album was a cringe-fest of musical proportions!', NOW());
 
