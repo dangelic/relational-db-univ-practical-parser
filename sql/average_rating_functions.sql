2491c3ccf3c5865c9bfe5207f316c1f77b2f37f6
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-06-29 11:44:31 +0200

    #feat Update average_rating_functions.sql

--- sql/average_rating_functions.sql
+++ sql/average_rating_functions.sql
@@ -1,55 +1,60 @@
--- TASK 2b)
--- This code below implements the functionality to keep average_rating integrity if a new userreview or guestreview gets added (or deleted)
+-- TESTAT II
+-- dbprak21
+-- b) Code (Beispiel in anderem File)
 
--- A PL/pgsql-function is used:
---      Fills the the average_rating column for each product in the "products" table
---      Fetches data from both "userreviews" and "guestreviews" tables to calculate the average rating over a UNION.
---      This function is on UPDATE of the reviews' tables - as defined by triggers below.
+-- Der folgende Code implementiert die Funktionalität, um die Integrität der Durchschnittsbewertung ("average_rating") aufrechtzuerhalten, wenn eine neue Benutzerbewertung ("userreview") oder Gästebewertung ("guestreview") hinzugefügt oder gelöscht wird.
+    -- Eine PL/pgsql-Funktion wird verwendet:
+    -- Die Funktion füllt die Spalte "average_rating" für jedes Produkt in der Tabelle "products".
+    -- Dabei werden Daten aus den Tabellen "userreviews" und "guestreviews" abgerufen, um die Durchschnittsbewertung über eine UNION-Abfrage zu berechnen.
+    -- Diese Funktion wird bei Aktualisierung der Bewertungstabellen durch Trigger aufgerufen.
 CREATE OR REPLACE FUNCTION calculate_average_ratings_ontrigger()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Perform an UPDATE statement on the "products" table
+    -- Führe ein UPDATE-Statement auf der Tabelle "products" aus.
     UPDATE products
     SET average_rating = (
-        -- Calculate the average rating using a subquery
+        -- Berechne die Durchschnittsbewertung mithilfe einer Unterabfrage.
         SELECT AVG(rating)
         FROM (
-            -- Combine the ratings from both "userreviews" and "guestreviews" tables for a specific product.
+            -- Kombiniere die Bewertungen aus den Tabellen "userreviews" und "guestreviews" für ein bestimmtes Produkt.            
             SELECT rating FROM userreviews WHERE products_asin = products.asin
             UNION ALL
             SELECT rating FROM guestreviews WHERE products_asin = products.asin
         ) AS subquery
     );
-    -- Return the NEW row (required for trigger function).
+    --  Gib die aktualisierte Zeile (NEW) zurück (wird für die Trigger-Funktion benötigt).
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
--- Add on-update-trigger for guestreviews
+-- Add on-update-trigger für guestreviews
 CREATE TRIGGER update_average_rating_on_update_guestreviews_trigger
 AFTER INSERT ON guestreviews
 FOR EACH ROW
 EXECUTE FUNCTION calculate_average_ratings_ontrigger();
 
--- Add on-update-trigger for userreviews
+-- Add on-update-trigger für userreviews
 CREATE TRIGGER update_average_rating_on_update_userreviews_trigger
 AFTER INSERT ON userreviews
 FOR EACH ROW
 EXECUTE FUNCTION calculate_average_ratings_ontrigger();
 
--- Workaround:
---      To initially call the function without altering a value, we need the same function but with return type "void".
---      This function can be called with "SELECT calculate_average_ratings();" from the code to fill average_ratings initially.
+
+
+
+
+
+-- Workaround für initales Triggern der Funktion:
+    -- Um die Funktion initial aufzurufen, ohne einen Wert zu ändern, benötigen wir dieselbe Funktion, jedoch mit dem Rückgabetyp "void".
+    -- Diese Funktion kann mit "SELECT calculate_average_ratings();" aus dem Code aufgerufen werden, um die Durchschnittsbewertungen initial zu füllen.
 CREATE OR REPLACE FUNCTION calculate_average_ratings()
+    -- RETURN VOID!
 RETURNS void AS $$
 BEGIN
-    -- Perform an UPDATE statement on the "products" table
     UPDATE products
     SET average_rating = (
-        -- Calculate the average rating using a subquery
         SELECT AVG(rating)
         FROM (
-            -- Combine the ratings from both "userreviews" and "guestreviews" tables for a specific product
             SELECT rating FROM userreviews WHERE products_asin = products.asin
             UNION ALL
             SELECT rating FROM guestreviews WHERE products_asin = products.asin
